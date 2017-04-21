2e06ad279992da6b1a0cf68ade84179efa16b9d6
Victor Faria <voffbm@gmail.com>
2017-04-21 19:05:06 -0300

    generate_charts ok

--- src/ploter/generate_charts.sh
+++ src/ploter/generate_charts.sh
@@ -1,6 +1,5 @@
-names=`find ../results/ -name *.log`
-titles=`find ../results/ -name *.log | awk '{gsub(/results\//, ""); gsub(/\//, "_"); gsub(/\.\._/,"") ;print}'`
-
-# for each (name, title) do
-title='Sequential Full'; name=full; awk -v t="$title" 'BEGIN{print t}; /seconds time elapsed/ {gsub(",",".",$1); gsub(",", ".", $7); gsub("%", "/100", $7); print $1 "\t" $7 }' $name.log | python ../../ploter/ploter.py
-#done
+IFS=' ' read -r -a names  <<< `find ../results/ -name *.log`
+IFS=' ' read -r -a titles <<< `find ../results/ -name *.log | awk '{gsub(/results\//, ""); gsub(/\//, "_"); gsub(/\.\._/,""); gsub(/\.log/, ""); print}'`
+for i in "${!titles[@]}"; do
+    awk -v t="${titles[$i]}" 'BEGIN{print t}; /seconds time elapsed/ {gsub(",",".",$1); gsub(",", ".", $7); gsub("%", "/100", $7); print $1 "\t" $7 }' ${names[$i]} | python ploter.py
+done
